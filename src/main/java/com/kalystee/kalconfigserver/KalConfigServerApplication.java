2810fbe5c1a1c2be32f5f723d115c2924f3962e2
Kyleus <guillaume.thib02@gmail.com>
2023-08-18 11:55:45 -0400

    Add config store

---
+++ src/main/java/com/kalystee/kalconfigserver/KalConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.kalystee.kalconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class KalConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KalConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kalystee/kalconfigserver/KalConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalystee.kalconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
